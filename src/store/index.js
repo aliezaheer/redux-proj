30c3092c50c3b076fd2cdf46fe77e7e5014e1053
aliezaheer <alizaheer67@gmail.com>
2022-09-14 17:30:22 +0500

    store created

---
+++ src/store/index.js
@@ -0,0 +1,17 @@
+import { legacy_createStore } from "redux";
+
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === "INCR") {
+    return { counter: state.counter + 1 };
+  }
+};
+
+if (action.type === "DECR") {
+  return { counter: state.counter - 1 };
+}
+
+return state;
+
+const store = legacy_createStore(counterReducer);
+
+export default store;
