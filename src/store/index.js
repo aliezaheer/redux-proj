4a115596656ce921ddbd37e991570bf504bfe8b8
aliezaheer <alizaheer67@gmail.com>
2022-09-14 18:09:43 +0500

    setting first comp

--- src/store/index.js
+++ src/store/index.js
@@ -4,13 +4,13 @@ const counterReducer = (state = { counter: 0 }, action) => {
   if (action.type === "INCR") {
     return { counter: state.counter + 1 };
   }
+
+  if (action.type === "DECR") {
+    return { counter: state.counter - 1 };
+  }
+
+  return state;
 };
-
-if (action.type === "DECR") {
-  return { counter: state.counter - 1 };
-}
-
-return state;
 
 const store = legacy_createStore(counterReducer);
 
