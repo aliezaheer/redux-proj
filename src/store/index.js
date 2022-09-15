53aa32540ed13623e7f6523175cd6cee5c338ff1
aliezaheer <alizaheer67@gmail.com>
2022-09-15 20:55:26 +0500

    setting up react toolkit

--- src/store/index.js
+++ src/store/index.js
@@ -1,18 +1,41 @@
 import { legacy_createStore } from "redux";
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialValue = { counter: 0, showCounter: true };
 
+createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increse(state, action) {
+      state.counter = state.counter + action.payload;
+    },
+    toggleCounter() {
+      state.counter = !state.showcounter;
+    },
+  },
+});
+
 const counterReducer = (state = initialValue, action) => {
   if (action.type === "INCR") {
-    return { counter: state.counter + 1 };
+    return { counter: state.counter + 1, showCounter: state.showCounter };
   }
 
   if (action.type === "INCR_BY_5") {
-    return { counter: state.counter + action.payload };
+    return {
+      counter: state.counter + action.payload,
+      showCounter: state.showCounter,
+    };
   }
 
   if (action.type === "DECR") {
-    return { counter: state.counter - 1 };
+    return { counter: state.counter - 1, showCounter: state.showCounter };
   }
 
   if (action.type === "TOGGLE") {
